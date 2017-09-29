28a7edf5e421370375b240a95746113cd17e51fb
Andres CT <21222884+Andres-CT@users.noreply.github.com>
2017-09-29 00:59:59 -0500

    Update AI.py

--- AI.py
+++ AI.py
@@ -137,99 +137,106 @@ def checkWinTopRight(row, col, player_number):
     return False
 
 #ARBOLES#############
-
+    
 def checkAnySquare(BOARD, column, row, playerN):
    if(BOARD[row][column+1] == playerN and BOARD[row+1][column] == playerN and BOARD[row+1][column+1] == playerN):
-       if(BOARD[row+2][column]==0 or BOARD[row+2][column+1]==0 or Board[row][column-1]==0 or Board[row][column+2]==0):
+       if(BOARD[row+2][column]==0 or BOARD[row+2][column+1]==0):
            return True
    return False
 
-def checkAnyCone(BOARD, column, row, playerN):
-    if(column <= 5):
-      if(BOARD[row+1][column+1]== playerN and BOARD[row][column+2]== playerN):
-          return True
-      return False
-    return False
-
 def checkL1(BOARD,row,col,turn):
     global  width, height
-    if (col + 1 == width or row == 0 or row + 1 == width): return False
-    if ((BOARD[row][col] and BOARD[row+1][col] and BOARD[row+1][col-1])==turn and BOARD[row][col-1]!=0 and BOARD[row][col+1]!=0 and BOARD[row+1][col+1]==0):
+    if (col + 1 == width or col == 0 or row + 2 >= height): return False
+    if ((BOARD[row][col] and BOARD[row+1][col] and BOARD[row+1][col-1])==turn and BOARD[row][col-1]!=0
+        and BOARD[row+2][col]==0):
         return True
     return False
-
 
 def checkL2(BOARD,row,col,turn):
-    global width, height
-    if (col + 1 == width or row == 0 or row + 1 == width): return False
-    if ((BOARD[row][col] and BOARD[row+1][col] and BOARD[row+1][col+1])==turn and BOARD[row][col+1]!=0 and BOARD[row][col-1]!=0 and BOARD[row+1][col-1]==0):
-        return True
-    return False
-
-def checkL3(BOARD, row,col,turn):
     global  width, height
-    if (col + 1 == width or row == 0 or row + 1 == width): return False
-    if ((BOARD[row][col] and BOARD[row][col+1] and BOARD[row+1][col+1])==turn and BOARD[row+1][col]!=0 and BOARD[row][col+2]==0):
+    if (col + 1 == width or row + 2 >= height): return False
+    if ((BOARD[row][col] and BOARD[row+1][col] and BOARD[row+1][col+1])==turn and BOARD[row][col+1]!=0
+        and BOARD[row+2][col]==0):
         return True
     return False
 
-def checkL4(BOARD, row,col,turn):
-    global width, height
-    if (col + 1 == width or row == 0 or row + 1 == width): return False
-    if ((BOARD[row][col] and BOARD[row][col-1] and BOARD[row+1][col-1])==turn and BOARD[row+1][col]!=0and BOARD[row][col-2]==0):
+def checkL3(BOARD,row,col,turn):
+    global  width, height
+    if (col + 1 == width or col == 0 or row + 1 >= height): return False
+    if ((BOARD[row][col] and BOARD[row][col+1] and BOARD[row+1][col])==turn and BOARD[row+1][col+1]!=0
+        and BOARD[row-1][col]==0):
         return True
     return False
 
-def checkHorizontal(BOARD, row, col, turn):
-    global width, height
-    if (col + 1 == width or row == 0 or row + 1 == width): return False
-    if ((BOARD[row][col] and BOARD[row][col+1] and BOARD[row][col+2])==turn and BOARD[row+1][col+1]!=0):
+def checkL4(BOARD,row,col,turn):
+    global  width, height
+    if (col + 2 >= width or row + 1 >= height): return False
+    if ((BOARD[row][col] and BOARD[row][col+1] and BOARD[row+1][col+1])==turn and BOARD[row+1][col]!=0
+        and BOARD[row][col+2]==0):
         return True
+    return False
+
+def checkAnyCone(BOARD, column, row, playerN):
+    if(column<width-2 and row<height-2):
+        if(BOARD[row+1][column+1]== playerN and BOARD[row][column+2]== playerN
+           and BOARD[row][column]==playerN and BOARD[row+1][column]!=0
+           and BOARD[row+1][column+2]!=0 and (BOARD[row+2][column]==0 or 
+           BOARD[row+2][column+2])==0):
+            return True
+        return False
     return False
 
 def checkVertical(BOARD, row, col, turn):
-    global width, height
-    if (col + 1 == width or row == 0 or row + 1 == width): return False
-    if (((BOARD[row][col] and BOARD[row+1][col] and BOARD[row+2][col])==turn) and ((BOARD[row][col-1]!=0) or (BOARD[row][col+1]!=0)) and ((BOARD[row+1][col-1]==0)or (BOARD[row+1][col+1]==0))):
-        return True
-    return False
-
+     global width, height
+     if (col + 1 == width or col == 0 or row + 2 >= height): return False
+     if (((BOARD[row][col] and BOARD[row+1][col] and BOARD[row+2][col])==turn) 
+         and ((BOARD[row][col-1]!=0) or (BOARD[row+1][col-1]==0)) and ((BOARD[row][col+1]!=0) or (BOARD[row+1][col+1]!=0))):
+         return True
+     return False
+ 
+def checkZetas(BOARD, row, col, turn):
+     global width, height
+     if (col + 2 >= width or col - 2 <= 0 or row + 2 >= height): return False
+     if (((BOARD[row][col] and BOARD[row][col+1] and BOARD[row+1][col+1] 
+     and BOARD[row+1][col+2]) ==turn) and (BOARD[row][col+2]!=0) 
+         and ((BOARD[row+1][col]==0) or (BOARD[row+2][col+1]==0))):
+         return True
+     if (((BOARD[row][col] and BOARD[row][col-1] and BOARD[row+1][col-1] 
+     and BOARD[row+1][col-2]) ==turn) and (BOARD[row][col-2]!=0) 
+         and ((BOARD[row+1][col]==0) or (BOARD[row+2][col-1]==0))):
+         return True
+     return False
+ 
 def checkDiagonales(BOARD, row, col, turn):
     global width, height
-    if (col + 1 == width or row == 0 or row + 1 == width): return False
-    if ((BOARD[row][col] and BOARD[row+1][col+1] and BOARD[row+2][col+2])==turn) and (BOARD[row][col+1]!=0) and (BOARD[row][col+2]!=0)and (BOARD[row+1][col]!=0) and (BOARD[row+1][col+2]!=0):
+    if (col + 2 >= width or col - 2 <= 0 or row + 2 >= height): return False
+    if (((BOARD[row][col] and BOARD[row+1][col+1] and BOARD[row+2][col+2])==turn) 
+    and (BOARD[row][col+1]!=0) and (BOARD[row][col+2]!=0)and (BOARD[row+1][col]!=0) 
+    and (BOARD[row+1][col+2]!=0)):
         return True
-    if ((BOARD[row][col] and BOARD[row+1][col-1] and BOARD[row+2][col-2])==turn) and (BOARD[row][col-1]!=0) and (BOARD[row][col-2]!=0)and (BOARD[row+1][col]!=0) and (BOARD[row+1][col-2]!=0):
-        return True
-    return False
-
-def checkZetas(BOARD, row, col, turn):
-    global width, height
-    if (col + 1 == width or row == 0 or row + 1 == width): return False
-    if (((BOARD[row][col] and BOARD[row][col+1] and BOARD[row+1][col+1] and BOARD[row+1][col+2]) ==turn) and (BOARD[row][col+2]!=0) and ((BOARD[row+1][col]==0) or (BOARD[row+2][col+1]==0))):
-        return True
-    if (((BOARD[row][col] and BOARD[row][col-1] and BOARD[row+1][col-1] and BOARD[row+1][col-2]) ==turn) and (BOARD[row][col-2]!=0) and ((BOARD[row+1][col]==0) or (BOARD[row+2][col-1]==0))):
+    if (((BOARD[row][col] and BOARD[row+1][col-1] and BOARD[row+2][col-2])==turn) 
+    and (BOARD[row][col-1]!=0) and (BOARD[row][col-2]!=0)and (BOARD[row+1][col]!=0) 
+    and (BOARD[row+1][col-2]!=0)):
         return True
     return False
 
-
-def IntraCheckAnyT(BOARD, player_number):
+def IntraCheckAnyT(BOARD,player_number):
     for r in range(0, 6):
         for c in range(0, 7):
-            if (BOARD[r][c] == player_number):
-                if (IntraCheckWinBelow(r, c, player_number)
-                    or IntraCheckWinAbove(r, c, player_number)
-                    or IntraCheckLeft(r, c, player_number)
-                    or IntraCheckRight(r, c, player_number)
-                    or IntraCheckWinBottomRight(r, c, player_number)
-                    or IntraCheckWinBottomLeft(r, c, player_number)
-                    or IntraCheckWinTopLeft(r, c, player_number)
-                    or IntraCheckWinTopRight(r, c, player_number)):
+            if (BOARD[r][c]==player_number):
+                if (IntraCheckWinBelow(BOARD, r, c, player_number)
+                    or IntraCheckWinAbove(BOARD, r, c, player_number)
+                    or IntraCheckLeft(BOARD, r, c, player_number)
+                    or IntraCheckRight(BOARD, r, c, player_number)
+                    or IntraCheckWinBottomRight(BOARD, r, c, player_number)
+                    or IntraCheckWinBottomLeft(BOARD, r, c, player_number)
+                    or IntraCheckWinTopLeft(BOARD, r, c, player_number)
+                    or IntraCheckWinTopRight(BOARD, r, c, player_number)):
                     return True
     return False
 
 def IntraCheckWinBelow(BOARD,row, col, player_number):
-    if (col + 1 == 7 or row == 0 or row + 1 == 7): return False
+    if (col + 1 == 7 or row == 0 or row + 1 == 6): return False
     if (BOARD[row - 1][col + 1] == player_number and BOARD[row][col + 1] == player_number and BOARD[row + 1][
             col + 1] == player_number): return True
     return False
@@ -275,7 +282,7 @@ def IntraCheckWinTopRight(BOARD,row, col, player_number):
     return False
 
 def intelligentFunction1(turn, board):
-    i=0
+    i=random.randint(0,6)
     if (turn==1):
         for x in range(height-1,-1,-1):
             for y in range(0,width):
@@ -291,38 +298,36 @@ def intelligentFunction1(turn, board):
     else:
         i=random.randint(0,6)
         Q=ConectaT(board,0,turn,random.randint(0,6))
-        i=Q.CPT
-        """
-        start=1
-        for u in range(0,width):
-            if(board[0][u]!=0):
-                start=0
-                u=width
-        if(start==1):
-            i=random.randint(8,9)
-            if(i==8):
-                i=0
+        i=Q.CPT##MINIMAX
+        if(i<0):
+            i=random.randint(0,6)
+            start=1
+            for u in range(0,width):
+                if(board[0][u]!=0):
+                    start=0
+                    u=width
+            if(start==1):
+                i=random.randint(8,9)
+                if(i==8):
+                    i=0
+                else:
+                    i=6
             else:
-                i=6
-        else:
-            #i=random.randint(0,6)
-            #while (board[i][5]!=0):
-                #i=random.randint(0,6)
-            for x in range(0,height-1):
-                for y in range(0,width):
-                    if(board[x][y]!=turn and board[x][y]!=0 and board[x+1][y]==0):
-                        i=y
-                        for r in range(0,height-2):
-                            for c in range(0,width-2):
-                                if(board[r][c]==turn
-                                and board[r][c+1]==turn
-                                and board[r][c+2]==turn
-                                and board[r+1][c+1]==0):
-                                    i=c+1
-            if(board[5][i]!=0):
-               i=random.randint(0,6)
-               while (board[5][i]!=0):
-                   i=random.randint(0,6)"""
+                for x in range(0,height-1):
+                    for y in range(0,width):
+                        if(board[x][y]!=turn and board[x][y]!=0 and board[x+1][y]==0):
+                            i=y
+                            for r in range(0,height-2):
+                                for c in range(0,width-2):
+                                    if(board[r][c]==turn
+                                    and board[r][c+1]==turn
+                                    and board[r][c+2]==turn
+                                    and board[r+1][c+1]==0):
+                                        i=c+1
+                if(board[5][i]!=0):
+                   i=random.randint(0,6)
+                   while (board[5][i]!=0):
+                       i=random.randint(0,6)
     return i
 
 def intelligentFunction2(turn, board):
@@ -331,24 +336,26 @@ def intelligentFunction2(turn, board):
         i=random.randint(0,6)
     return i
 
-def placeChange(List,n,turn):
+def placeChange(alist,n,turn):
     change=0
-    IntraBoardx=copy.deepcopy(List)
+    IntraBoardx=copy.deepcopy(alist)
     for up in range(0,6):
-        if(List[up][n]==0):
+        if(alist[up][n]==0):
             IntraBoardx[up][n]=turn
             change=1
             return IntraBoardx
             break
         if(up==5 and change==0):
-            return 0
+            #print ("#############")
+            return [[0 for y in range(width)] for x in range(height)]
+            break
 
 class ConectaT:
     def __init__(self, intraBoard, depth, turn, columnaPtirar):
         self.IntraBoard = intraBoard
         self.depth = depth
         self.Turn = turn
-        self.Pointers=[]
+        self.Pointers = []
         self.CPT=columnaPtirar
         if(depth==0):
             turn=1+(turn%2)
@@ -361,27 +368,53 @@ class ConectaT:
         if(depth<=2):#-1 of desired level
             for k in range(0,7):
                 self.Pointers.append(ConectaT(placeChange(intraBoard, k, 1+(turn%2)),depth+1,1+(turn%2),k))
-        self.CPT=MejorTiro(self)
-
+        if(depth==0):
+            self.CPT=MejorTiro(self)
+        
 def Evaluate(BOARD,playerN):
-    Score=3
-    if(IntraCheckAnyT(playerN)):
+    Score=0
+    if(IntraCheckAnyT(BOARD,playerN)):
         Score=10000
-    if(IntraCheckAnyT(1+(playerN%2))):
+    if(IntraCheckAnyT(BOARD,1+(playerN%2))):
         Score=-10000
-    if(Score!=10000 and Score!=-10000):
+    if(Score!=10000 and Score!=(-10000)):
         for r in range(0,height-2):
             for c in range(0,width-1):
                 if(checkAnySquare(BOARD,c,r,playerN)):
-                    Score+=100
+                    Score+=170
+        for r in range(0,height-2):
+            for c in range(0,width-2):
+                if(BOARD[r][c]==playerN
+                   and BOARD[r][c+1]==playerN
+                   and BOARD[r][c+2]==playerN
+                   and BOARD[r+1][c+1]==0):
+                    Score+=125
+        for r in range(0,height):
+            for c in range(0,width):
+                if(checkL1(BOARD,r,c,playerN)):
+                    Score+=60
+                if(checkL2(BOARD,r,c,playerN)):
+                    Score+=60
+                if(checkL3(BOARD,r,c,playerN)):
+                    Score+=50
+                if(checkL4(BOARD,r,c,playerN)):
+                    Score+=50
+                if(checkAnyCone(BOARD,c,r,playerN)):
+                    Score+=150
+                if(checkVertical(BOARD,r,c,playerN)):
+                    Score+=80
+                if(checkZetas(BOARD,r,c,playerN)):
+                    Score+=130
+                if(checkDiagonales(BOARD,r,c,playerN)):
+                    Score+=120
     return Score
 
 def MejorTiro(object):
     MejorScore=0
     Candidato=0
-    ElMejorTiro=0
-    NoTirar=[]
-    Fail=0
+    ElMejorTiro=-1
+    while (board[5][ElMejorTiro]!=0):
+        ElMejorTiro=random.randint(0,6)
     for ii in range(0,7):
         if(Evaluate(object.Pointers[ii].IntraBoard,object.Pointers[ii].Turn)==10000):
             ElMejorTiro=object.Pointers[ii].CPT
@@ -390,30 +423,27 @@ def MejorTiro(object):
     for ii in range(0,7):
         for jj in range(0,7):
             if(Evaluate(object.Pointers[ii].Pointers[jj].IntraBoard,object.Pointers[ii].Pointers[jj].Turn)==10000):
-                NoTirar.append(object.Pointers[ii].Pointers[jj].CPT)
+                ElMejorTiro=object.Pointers[ii].Pointers[jj].CPT
+                MejorScore=10000
+                break
     if(MejorScore!=10000):
         for ii in range(0,7):
-            for NOUSES in range(0,len(NoTirar)):
-                if(ii==NoTirar[NOUSES]):
-                    Fail=1
-                    break
-                if(Fail==0):
-                    for jj in range(0,7):
-                        for hh in range(0,7):
-                            Candidato=(Evaluate(object.Pointers[ii].Pointers[jj].Pointers[hh].IntraBoard,object.Pointers[ii].Pointers[jj].Pointers[hh].Turn))
-                            if(Candidato>MejorScore):
-                                ElMejorTiro=(object.Pointers[ii].Pointers[jj].Pointers[hh].CPT)
-                                MejorScore=Candidato
+            for jj in range(0,7):
+                for hh in range(0,7):
+                    Candidato=(Evaluate(object.Pointers[ii].Pointers[jj].Pointers[hh].IntraBoard,object.Pointers[ii].Pointers[jj].Pointers[hh].Turn))
+                    if(Candidato>MejorScore and board[5][object.Pointers[ii].Pointers[jj].Pointers[hh].CPT]==0):
+                        ElMejorTiro=(object.Pointers[ii].Pointers[jj].Pointers[hh].CPT)
+                        MejorScore=Candidato
     return ElMejorTiro
-
 
 #ARBOLES#############
 
 def main():
     global board
+    """
     Kee="z"
     while(Kee!="a" and Kee!="b"):
-        print("¿Quién va a comenzar el juego?\n\ta)Un humano\n\tb)El programa")
+        print("¿Quién va a comenzar el juego?\n\ta) Un humano\n\tb) El programa")
         Kee=input()
         if(Kee=="a" or Kee=="b"):
             if(Kee=="a"):
@@ -421,27 +451,28 @@ def main():
             else:
                 turn=1
         else:
-            print("Caracter inválido")
+            print("Caracter inválido")"""
+    turn=1
     loser = 0
     while (gameFinished(turn)==0):
         printGame()
         turn=1+(turn%2)
         if (turn == 1):
-            column = int(input("Columna para tirar: "))
+            column = intelligentFunction2(turn, board)
+            #column = int(input("Columna para tirar: "))
         if (turn == 2):
             column = intelligentFunction1(turn, board)
+            print ("El programa tiró en: ",column)
         if (place(column, turn) == -1):
             loser = turn
             break
 
     # Game is a tie
     if (gameFinished(turn) == -1): print("The game is a tie!")
-    elif not (loser == 0): print ("The loser is ", turn)
+    elif not (loser == 0): print ("The loser is", turn)
     else:
         printGame()
-        print ("The winner is ", turn)
-
-
+        print ("The winner is", turn)
 
 if __name__ == '__main__':
     main()
